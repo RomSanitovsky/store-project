7da69406f0f428d3b07e7a87f9edd179785bc762
רום סניטובסקי <romrom306@gmail.com>
2020-09-30 21:31:40 +0300

    master

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+// const text=fs.readFileSync('./txt/input.txt','utf-8');
+// console.log(text);
+// fs.writeFileSync('./txt/output.txt',`this is the text:\n${text}`);
+
+const server = http.createServer((req , res) =>
+{
+    console.log('start');
+    res.end('hello from the server');
+});
+
+server.listen(8000,"127.0.0.1", ()=>
+{
+    console.log('lisening...');
+});
